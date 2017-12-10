0a3df1c8b192ce741a0cc7f8894ea9390c6d37fa
adrake33 <aarondrake@gmail.com>
2017-12-09 20:15:51 -0600

    Adding/updating JSDoc documentation

--- src/trading/get-better-worse-orders.js
+++ src/trading/get-better-worse-orders.js
@@ -1,7 +1,23 @@
 "use strict";
 
+/**
+ * @typedef BetterWorseOrders
+ * @param {string|null} betterOrderID ID of the order with the better price, as a hexadecimal string.
+ * @param {string|null} worseOrderID ID of the order with the worse price, as a hexadecimal string.
+ */
+
 var augurNode = require("../augur-node");
 
+/**
+ * Returns the IDs of the orders for a given outcome that have a better and worse price than the specified price. If no better/worse orders exist, null will be returned. This function should be called prior to calling augur.api.CreateOrder.publicCreateOrder in order to get the _betterOrderId and _worseOrderId parameters that it accepts. (_betterOrderId and _worseOrderId are used to optimize the sorting of Orders on the Order Book.) Requires an Augur Node connection.
+ * @param {Object} p Parameters object.
+ * @param {string} p.marketID Contract address of the market for which to retrieve the better/worse orders, as a hexadecimal string.
+ * @param {string} p.outcome Market outcome for which to find better/worse orders.
+ * @param {string} p.orderType Desired type of order. Valid values are "buy" and "sell".
+ * @param {number} p.price Price point at which to find better/worse orders.
+ * @param {function} callback Called when better/worse orders have been retrieved.
+ * @return {BetterWorseOrders} Object containing the better/worse order IDs, as hexidecimal strings.
+ */
 function getBetterWorseOrders(p, callback) {
   augurNode.submitRequest("getBetterWorseOrders", p, callback);
 }
