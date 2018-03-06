d5deaf208c5edb63cde27ef46b407b5e93fd356e
adrake33 <aarondrake@gmail.com>
2018-03-06 13:52:27 -0800

    Updating index.js for reporting functions

--- src/reporting/index.js
+++ src/reporting/index.js
@@ -2,9 +2,8 @@
 
 module.exports = {
   getReportingHistory: require("./get-reporting-history"),
-  getStakeTokens: require("./get-stake-tokens"),
   getReportingSummary: require("./get-reporting-summary"),
-  getFeeWindowsWithUnclaimedFees: require("./get-fee-windows-with-unclaimed-fees"),
+  getFeeWindows: require("./get-fee-windows"),
   getFeeWindowCurrent: require("./get-fee-window-current"),
   getDisputeInfo: require("./get-dispute-info"),
   getStakeRequiredForDesignatedReporter: require("./get-stake-required-for-designated-reporter"),
