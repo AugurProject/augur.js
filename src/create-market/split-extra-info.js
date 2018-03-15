d95a42ccd602b1db50c4a1d4b35af82b11e273d6
Scott Bigelow <epheph@gmail.com>
2018-03-15 16:06:36 -0700

    looks like those prices aren't in extraInfo any more?

--- src/create-market/split-extra-info.js
+++ src/create-market/split-extra-info.js
@@ -4,14 +4,14 @@ var assign = require("lodash.assign");
 var immutableDelete = require("immutable-delete");
 
 function splitExtraInfo(p) {
-  var extraInfoKeys = ["longDescription", "tags", "minPrice", "maxPrice", "_scalarDenomination"];
+  var extraInfoKeys = ["longDescription", "tags", "_scalarDenomination", "resolutionSource", "outcomeNames"];
   var extraInfo = extraInfoKeys.reduce(function (prev, extraInfoKey) {
     if (p.hasOwnProperty(extraInfoKey) && !prev.hasOwnProperty(extraInfoKey)) prev[extraInfoKey] = p[extraInfoKey];
     return prev;
   }, assign({}, p._extraInfo));
   return assign({},
     immutableDelete(p, extraInfoKeys),
-    {_extraInfo: extraInfo}
+    { _extraInfo: extraInfo }
   );
 }
 
