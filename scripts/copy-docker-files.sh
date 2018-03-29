5e1cf7d9836ec18c70748ffb6b3e0498b6c653d6
Tom Haile <bradford.haile@gmail.com>
2018-03-29 13:58:10 -0500

    use exit codes to kill copy script and show error

--- scripts/copy-docker-files.sh
+++ scripts/copy-docker-files.sh
@@ -6,11 +6,23 @@ ADDRESSES="./src/contracts/addresses.json"
 BLOCKS="./src/contracts/upload-block-numbers.json"
 
 docker run --rm --entrypoint cat $IMAGE /augur.js/src/contracts/addresses.json > $TEMP1
-node ./scripts/merge-json-files -p $ADDRESSES -s $TEMP1 > $TEMP2
+node ./scripts/merge-json-files -p $ADDRESSES -s $TEMP1 -o $TEMP2
+
+if [ $? -eq 1 ]; then
+ echo 'ERROR occurred in updating Addresses.json file'
+ exit
+fi
+
 cat $TEMP2 > $ADDRESSES
 rm -rf $TEMP1 $TEMP2
 
 docker run --rm --entrypoint cat $IMAGE /augur.js/src/contracts/upload-block-numbers.json > $TEMP1
-node ./scripts/merge-json-files -p $BLOCKS -s $TEMP1 > $TEMP2
+node ./scripts/merge-json-files -p $BLOCKS -s $TEMP1 -o $TEMP2
+
+if [ $? -eq 1 ]; then
+ echo 'ERROR occurred in updating upload-block-numbers.json file'
+ exit
+fi
+
 cat $TEMP2 > $BLOCKS
 rm -rf $TEMP1 $TEMP2
--- scripts/merge-json-files.js
+++ scripts/merge-json-files.js
@@ -18,20 +18,25 @@ var fileType = "utf8";
 var opts = {
   primary: { required: true, short: "p", help: "Existing Nework addresses.json file"},
   secondary: { required: true, short: "s", help: "Addresses.json file to merge into main addresses.json file"},
+  output: { required: true, short: "o", help: "output file of the merge"},
 };
 var args = options.parse(opts, process.argv, function (error) {
   errorOccurred(error, opts);
 });
 var primaryFile = args.opt.primary;
 var secondaryFile = args.opt.secondary;
+var output = args.opt.output;
 fs.readFile(primaryFile, fileType, function (err, primaryContent) {
   errorOccurred(err, opts);
   if (!primaryContent || primaryContent.length === 0) return console.error("existing addresses.json file has no content");
   fs.readFile(secondaryFile, fileType, function (err, secondaryContent) {
     errorOccurred(err, opts);
     if (!secondaryContent || secondaryContent.length === 0) return console.error("new addresses.json file has no content");
-    console.log(mergeJsonFiles(primaryContent, secondaryContent));
-    process.exit(0);
+    var result = mergeJsonFiles(primaryContent, secondaryContent);
+    fs.writeFile(output, result, function (err) {
+      errorOccurred(err, opts);
+      process.exit(0);
+    });
   });
 });
 
