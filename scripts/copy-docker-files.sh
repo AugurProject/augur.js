e049cf7fb7194f0312ecb1b262f4e65af475310d
Tom Haile <bradford.haile@gmail.com>
2018-03-29 14:04:54 -0500

    better error handling when merge json exits with error

--- scripts/copy-docker-files.sh
+++ scripts/copy-docker-files.sh
@@ -1,28 +1,29 @@
 # copy files so augur.js is consistant with docker image
 IMAGE="augurproject/dev-pop-geth:latest"
 TEMP1="./temp.file"
-TEMP2="./temp2.file"
+ADD_TEMP2="./temp2.file"
+BLOCK_TEMP2=".temp3.file"
 ADDRESSES="./src/contracts/addresses.json"
 BLOCKS="./src/contracts/upload-block-numbers.json"
 
 docker run --rm --entrypoint cat $IMAGE /augur.js/src/contracts/addresses.json > $TEMP1
-node ./scripts/merge-json-files -p $ADDRESSES -s $TEMP1 -o $TEMP2
+node ./scripts/merge-json-files -p $ADDRESSES -s $TEMP1 -o $ADD_TEMP2
+ADD_RESULT_CODE=$?
 
-if [ $? -eq 1 ]; then
+docker run --rm --entrypoint cat $IMAGE /augur.js/src/contracts/upload-block-numbers.json > $TEMP1
+node ./scripts/merge-json-files -p $BLOCKS -s $TEMP1 -o $BLOCK_TEMP2
+ADD_BLOCK_CODE=$?
+
+if [ $ADD_RESULT_CODE -eq 1 ]; then
  echo 'ERROR occurred in updating Addresses.json file'
  exit
 fi
 
-cat $TEMP2 > $ADDRESSES
-rm -rf $TEMP1 $TEMP2
-
-docker run --rm --entrypoint cat $IMAGE /augur.js/src/contracts/upload-block-numbers.json > $TEMP1
-node ./scripts/merge-json-files -p $BLOCKS -s $TEMP1 -o $TEMP2
-
-if [ $? -eq 1 ]; then
+if [ $ADD_BLOCK_CODE -eq 1 ]; then
  echo 'ERROR occurred in updating upload-block-numbers.json file'
  exit
 fi
 
-cat $TEMP2 > $BLOCKS
-rm -rf $TEMP1 $TEMP2
+cat $ADD_TEMP2 > $ADDRESSES
+cat $BLOCK_TEMP2 > $BLOCKS
+rm -rf $TEMP1 $ADD_TEMP2 $BLOCK_TEMP2
