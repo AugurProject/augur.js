9da02c878683a0ee2664c4861bd0de3525dbbf95
adrake33 <aarondrake@gmail.com>
2018-07-30 13:58:13 -0700

    Added better error handling for startAugurNodeEventListeners

--- src/events/start-augur-node-event-listeners.js
+++ src/events/start-augur-node-event-listeners.js
@@ -9,12 +9,13 @@ var noop = require("../utils/noop");
 /**
  * Start listening for events emitted by augur-node.
  * @param {Object.<function>} eventCallbacks Callbacks to fire when events are received, keyed by event name.
- * @param {function=} onSetupComplete Called when all listeners are successfully set up.
+ * @param {function=} onSetupComplete Called when all listeners are successfully set up, or when an error is encountered while setting up the listeners.
  */
 function startAugurNodeEventListeners(eventCallbacks, onSetupComplete) {
   if (!isFunction(onSetupComplete)) onSetupComplete = noop;
   if (!isObject(eventCallbacks)) return onSetupComplete(new Error("No callbacks found"));
   async.forEachOf(eventCallbacks, function (callback, eventName, nextEvent) {
+    if (typeof callback !== "function") return nextEvent(new Error("Callback for " + eventName + " is not a function"));
     augurNode.subscribeToEvent(eventName, callback, nextEvent);
   }, onSetupComplete);
 }
