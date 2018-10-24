fce97b30507a0d6c50a6d4a0e8023a60f79b1481
Paul Gebheim <paul@onetaste.us>
2018-10-24 18:34:14 +0000

    Remove unused imports

--- src/augur-node/disconnect.js
+++ src/augur-node/disconnect.js
@@ -1,8 +1,6 @@
 "use strict";
 
 var augurNodeState = require("./state");
-var dispatchJsonRpcResponse = require("./dispatch-json-rpc-response");
-var WsTransport = require("../rpc-interface").WsTransport;
 
 function disconnect() {
   if (augurNodeState.getTransport() !== null) augurNodeState.getTransport().close();
