121a58bb52a910141620101cbd69eced54b2782f
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-03-08 16:10:44 -0800

    Increase augur node http transport to be in parity with eth transport
    This is 60000 or 1 minute for those playing at home.

--- src/augur-node/AugurNodeHttpTransport.js
+++ src/augur-node/AugurNodeHttpTransport.js
@@ -2,6 +2,8 @@ var HttpTransport = require("../rpc-interface").HttpTransport;
 
 function AugurNodeHttpTransport(address, timeout, maxRetries, messageHandler, initialConnectCallback) {
   HttpTransport.call(this, address, timeout, maxRetries, messageHandler, initialConnectCallback);
+
+  this.timeout = 60000;
 }
 
 AugurNodeHttpTransport.prototype = Object.create(HttpTransport.prototype);
