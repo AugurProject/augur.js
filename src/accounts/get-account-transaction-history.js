d8337a63f8fd32582a3cf6fd89af02d42a0b5560
adrake33 <aarondrake@gmail.com>
2019-03-22 02:41:10 -0700

    JSDoc changes

--- src/accounts/get-account-transaction-history.js
+++ src/accounts/get-account-transaction-history.js
@@ -1,19 +1,20 @@
 "use strict";
 
 /**
- * @typedef {Object} AccountTransfer
- * @property {string} transactionHash Hash returned by token transfer.
- * @property {number} logIndex Number of the log index position in the Ethereum block containing the transfer.
- * @property {number} creationBlockNumber Number of the Ethereum block containing the transfer.
- * @property {string} blockHash Hash of the Ethereum block containing the transfer.
- * @property {number} creationTime Timestamp, in seconds, when the Ethereum block containing the transfer was created.
- * @property {string|null} sender Ethereum address of the token sender. If null, this indicates that new tokens were minted and sent to the user.
- * @property {string|null} recipient Ethereum address of the token recipient. If null, this indicates that tokens were burned (i.e., destroyed).
- * @property {string} token Contract address of the contract for the sent token, as a hexadecimal string.
- * @property {number} value Quantity of tokens sent.
- * @property {string|null} symbol Token symbol (if any).
- * @property {number|null} outcome Market outcome with which the token is associated (if any).
- * @property {string|null} marketId Contract address of the market in which the tranfer took place, as a hexadecimal string (if any).
+ * @typedef {Object} AccountTransaction
+ * @property {Action} action
+ * @property {string} coin
+ * @property {string} costBasis
+ * @property {string} details
+ * @property {string} fee
+ * @property {string} marketDescription
+ * @property {string} marketId
+ * @property {string} outcome
+ * @property {string} outcomeDescription
+ * @property {string} quantity
+ * @property {number} timestamp
+ * @property {string} total
+ * @property {string} transactionHash
  */
 
 var augurNode = require("../augur-node");
@@ -25,8 +26,8 @@ var augurNode = require("../augur-node");
  * @param {string} p.account Ethereum address of the account for which to get transaction history, as a hexadecimal string.
  * @param {number} p.earliestTransactionTime Earliest timestamp, in seconds, at which to truncate history results. (This timestamp is when the block on the Ethereum blockchain containing the transaction was created.)
  * @param {number} p.latestTransactionTime Latest timestamp, in seconds, at which to truncate history results. (This timestamp is when the block on the Ethereum blockchain containing the transaction was created.)
- * @param {Denomination} p.denomination Cryptocurrency denomination by which to restrict history results.
- * @param {ActionType=} p.actionType Transaction type by which to restrict history results.
+ * @param {Coin} p.coin Cryptocurrency denomination by which to restrict history results.
+ * @param {Action=} p.action Transaction type by which to restrict history results.
  * @param {string=} p.sortBy Field name by which to sort transaction history.
  * @param {boolean=} p.isSortDescending Whether to sort transactions in descending order by sortBy field.
  * @param {string=} p.limit Maximum number of transactions to return.
